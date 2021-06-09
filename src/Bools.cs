d85e43c7bca0ba6548ffdc66993d6565cc6f689f
AureumApes <76166556+AureumApes@users.noreply.github.com>
2021-06-09 14:14:58 +0200

    Clean Code up

--- src/Bools.cs
+++ src/Bools.cs
@@ -7,24 +7,27 @@ namespace ImageVericationLib
     public class Bools
     {
         public static bool CheckSize(Image image, int height, int with){
-            if(height == image.Height && with == image.Width){
+            if(height == image.Height && with == image.Width)
                 return true;
-            }
-            return false;
+            else
+                return false;
         }
 
         public static bool CheckType(Image image, String type){
-            if(type == "JPEG" && image.RawFormat.Equals(ImageFormat.Jpeg)) return true;
-            if(type == "BMP" && image.RawFormat.Equals(ImageFormat.Bmp)) return true;
-            if(type == "PNG" && image.RawFormat.Equals(ImageFormat.Png)) return true;
+            if(type == "JPEG" && image.RawFormat.Equals(ImageFormat.Jpeg)) 
+                return true;
+            if(type == "BMP" && image.RawFormat.Equals(ImageFormat.Bmp))
+                return true;
+            if(type == "PNG" && image.RawFormat.Equals(ImageFormat.Png)) 
+                return true;
             return false;
         }
 
         public static bool CheckDepth(Image image, int depth){
-            if(depth == Image.GetPixelFormatSize(image.PixelFormat)){
+            if(depth == Image.GetPixelFormatSize(image.PixelFormat))
                 return true;
-            }
-            return false;
+            else
+                return false;
         }
     }
 }
