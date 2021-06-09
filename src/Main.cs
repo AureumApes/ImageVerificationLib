7d104c8580e4be08bcc2e9f7ed907ec5a412bef3
AureumApes <76166556+AureumApes@users.noreply.github.com>
2021-06-09 14:12:35 +0200

    Update Main.cs

--- src/Main.cs
+++ src/Main.cs
@@ -6,22 +6,12 @@ namespace ImageVericationLib
     public class Main
     {
         public static bool CheckImage(String path, String type, int height, int width, int depth){
-            // ----------------------------------------------------------------------------------------------------
             Image img = Image.FromFile(path);
-            // ----------------------------------------------------------------------------------------------------
-            // Check if Image Size is right!
-            if(!Bools.CheckSize(img, height, width)){
+            
+            if(Bools.CheckSize(img, height, width) && Bools.CheckDepth(img, depth) && Bools.CheckType(img, type))
+                return true;
+            else
                 return false;
-            }
-            // Check if ColorDepth is right!
-            if(!Bools.CheckDepth(img, depth)){
-                return false;
-            }
-            // Check if Image Type is right! (Without the Filename)
-            if(!Bools.CheckType(img, type)){
-                return false;
-            }
-            return true;
         }
     }
 }
